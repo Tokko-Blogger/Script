c86607db0cbbff96c769b6247c3c7096541d9ad5
Helmy Kambuna Otaya (Tokko Blogger) <21377149+twinnybloggers@users.noreply.github.com>
2024-01-25 22:55:57 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBcTsdNM48Ts1t5JTNdDby8281e7s7REDE",
+  authDomain: "simforum-login.firebaseapp.com",
+  databaseURL: "https://simforum-login-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "simforum-login",
+  storageBucket: "simforum-login.appspot.com",
+  messagingSenderId: "311465979099",
+  appId: "1:311465979099:web:8ded10990bee13bfb770fe",
+  measurementId: "G-WP0XZT3CHT"
+};
