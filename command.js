ed1bc2b384c970a484913bc637ebd604ea77a6e4
Helmy Kambuna Otaya (Tokko Blogger) <21377149+twinnybloggers@users.noreply.github.com>
2024-01-26 23:04:43 +0700

    Update command.js

--- command.js
+++ command.js
@@ -100,8 +100,9 @@ export const botCommand = (currentID) => {
                             <i class="fa-thin fa-user-robot"></i>
                         </span>
                         <span class="command-message">
+                        Welcome To <b>Tokko Chat</b> <a href='https://tokko-realtime.blogspot.com/'>@https://tokko-realtime.blogspot.com</a> if you need Please write down below:<br/><br/>
                             Type <span class="code">/help</span> for help<br/>
-                           Press <span class="code">CTRL + ;</span> Exit<br/>
+                           Press <span class="code">CTRL + ;</span> to Exit<br/>
                         </span>
                     </div>
                 </div>
@@ -162,31 +163,31 @@ export const botCommand = (currentID) => {
     const commandListener = (chatBox, input, send, command_box) => {
         const commands = {
             last_used: null,
-            username: {
+            nama: {
                 init: (args, line, bot, box) => {
                     commands.last_used = null;
                     line.innerHTML = `Thinking...`;
                     if(!args[0])  {
-                        line.innerHTML = `Usage:<br/>/username get<br/>/username set &lt;new_username>`;
+                        line.innerHTML = `Usage:<br/>/nama get<br/>/nama set &lt;new_nama>`;
                         return box.appendChild(bot);
                     }
 
                     if(args[0] == 'get') {
                         let uname;
                         get(ref(db, 'users/' + currentID)).then((data) => {
-                            uname = data.val().username || data.val().displayName;
+                            uname = data.val().nama || data.val().displayName;
                             line.innerHTML = `Your Name is <b>${uname.replace(/\</g, '&lt;')}</b>`;
                         });
                     } else if(args[0] == 'set') {
                         if(!args[1]) {
-                            line.innerHTML = `<span class="c-yellow">Enter Username</span>`;
+                            line.innerHTML = `<span class="c-yellow">Enter nama</span>`;
                         } else if(args[2]) {
                             line.innerHTML = `<span class="c-red">User name Has Not</span>`;
                         } else {
-                            update(ref(db, 'users/' + currentID), {username: args[1]}).then(() => {
-                                line.innerHTML = `<span class="c-green">Username Update <b>${args[1].replace(/\</g, '&lt;')}</b></span>`;
+                            update(ref(db, 'users/' + currentID), {nama: args[1]}).then(() => {
+                                line.innerHTML = `<span class="c-green">nama Update <b>${args[1].replace(/\</g, '&lt;')}</b></span>`;
                             }).catch((err) => {
-                                line.innerHTML = `<span class="c-red">Username Length</span>`;
+                                line.innerHTML = `<span class="c-red">nama Length</span>`;
                             })
                         }
                     } else {
@@ -195,19 +196,19 @@ export const botCommand = (currentID) => {
                     box.appendChild(bot);
                 }
             },
-            about: {
+            bio: {
                 init: (args, line, bot, box) => {
                     commands.last_used = null;
                     line.innerHTML = `Thinking...`;
                     if(!args[0])  {
-                        line.innerHTML = `Usage:<br/>/about get<br/>/about edit<br/>/about remove`;
+                        line.innerHTML = `Usage: <br/>/bio get<br/>/bio edit<br/>/bio remove`;
                         return box.appendChild(bot);
                     }
                     if(args[0] == 'get') {
                         get(ref(db, 'users/' + currentID)).then((data) => {
-                            if(data.val().about) {
-                                line.innerHTML = `Your About Section<br/><i class="code"></i>`;
-                                line.querySelector('.code').innerText = data.val().about;
+                            if(data.val().bio) {
+                                line.innerHTML = `Your bio Section<br/><i class="code"></i>`;
+                                line.querySelector('.code').innerText = data.val().bio;
                                 box.scrollTop = box.scrollHeight;
                             } else {
                                 line.innerHTML = `You Dont have any bio`;
@@ -217,14 +218,14 @@ export const botCommand = (currentID) => {
                     } else if(args[0] == 'edit') {
                         line.innerHTML = `<i>Waiting . . .</i>`;
                         popup.prompt({
-                            msg: 'About:',
+                            msg: 'bio:',
                             textarea: true,
                             max: 100,
                             onyes: (res) => {
                                 if(res.length > 150) {
                                     line.innerHTML = `<span class="c-red">Please Enter less</span>`;
                                 } else {
-                                    update(ref(db, 'users/' + currentID), {about: res}).then(() => {
+                                    update(ref(db, 'users/' + currentID), {bio: res}).then(() => {
                                         line.innerHTML = `<span class="c-green">update</span>`;
                                     }).catch((err) => {
                                         line.innerHTML = `<span class="c-red">please enter less</span>`;
@@ -232,12 +233,12 @@ export const botCommand = (currentID) => {
                                 }
                             },
                             onno: () => {
-                                line.innerHTML = `<span class="c-yellow">you cancled about}</span>`;
+                                line.innerHTML = `<span class="c-yellow">you cancled bio}</span>`;
                             }
                         })
                     } else if(args[0] == 'remove') {
-                        commands.last_used = 'about';
-                        line.innerHTML = `Type <span class="code">/confirm</span> to clear about`;
+                        commands.last_used = 'bio';
+                        line.innerHTML = `Type <span class="code">/confirm</span> to clear bio`;
                     } else {
                         line.innerHTML = `<span class="c-red"><b>${args[0].replace(/\</g, '&lt;')}</b> is not valid</span>`;
                     }
@@ -246,7 +247,178 @@ export const botCommand = (currentID) => {
                 commit: (args, line, bot, box) => {
                     commands.last_used = null;
                     line.innerHTML = `<span class="c-yellow">Removing...</span>`;
-                    remove(ref(db, 'users/' + currentID + '/about')).then(() => {
+                    remove(ref(db, 'users/' + currentID + '/bio')).then(() => {
+                        line.innerHTML = `<span class="c-green">Cleared</span>`;
+                    })
+                    box.appendChild(bot);
+                }
+            },
+            realname: {
+                init: (args, line, bot, box) => {
+                    commands.last_used = null;
+                    line.innerHTML = `Thinking...`;
+                    if(!args[0])  {
+                        line.innerHTML = `Usage: <br/>/realname get<br/>/realname edit<br/>/realname remove`;
+                        return box.appendChild(bot);
+                    }
+                    if(args[0] == 'get') {
+                        get(ref(db, 'users/' + currentID)).then((data) => {
+                            if(data.val().realname) {
+                                line.innerHTML = `Your realname Is<br/><i class="code"></i>`;
+                                line.querySelector('.code').innerText = data.val().realname;
+                                box.scrollTop = box.scrollHeight;
+                            } else {
+                                line.innerHTML = `You Dont have any realname`;
+                                box.scrollTop = box.scrollHeight;
+                            }
+                        });
+                    } else if(args[0] == 'edit') {
+                        line.innerHTML = `<i>Waiting . . .</i>`;
+                        popup.prompt({
+                            msg: 'Your Realname is:',
+                            textarea: true,
+                            max: 100,
+                            onyes: (res) => {
+                                if(res.length > 150) {
+                                    line.innerHTML = `<span class="c-red">Please Enter less</span>`;
+                                } else {
+                                    update(ref(db, 'users/' + currentID), {realname: res}).then(() => {
+                                        line.innerHTML = `<span class="c-green">update</span>`;
+                                    }).catch((err) => {
+                                        line.innerHTML = `<span class="c-red">Please enter less</span>`;
+                                    });
+                                }
+                            },
+                            onno: () => {
+                                line.innerHTML = `<span class="c-yellow">You cancled Edited Your Realname</span>`;
+                            }
+                        })
+                    } else if(args[0] == 'remove') {
+                        commands.last_used = 'realname';
+                        line.innerHTML = `Type <span class="code">/confirm</span> to clear realname`;
+                    } else {
+                        line.innerHTML = `<span class="c-red"><b>${args[0].replace(/\</g, '&lt;')}</b> is not valid</span>`;
+                    }
+                    box.appendChild(bot);
+                },
+                commit: (args, line, bot, box) => {
+                    commands.last_used = null;
+                    line.innerHTML = `<span class="c-yellow">Removing...</span>`;
+                    remove(ref(db, 'users/' + currentID + '/realname')).then(() => {
+                        line.innerHTML = `<span class="c-green">Cleared</span>`;
+                    })
+                    box.appendChild(bot);
+                }
+            },
+            pesan: {
+                init: (args, line, bot, box) => {
+                    commands.last_used = null;
+                    line.innerHTML = `Thinking...`;
+                    if(!args[0])  {
+                        line.innerHTML = `Usage: <br/>/pesan get<br/>/pesan edit<br/>/pesan remove`;
+                        return box.appendChild(bot);
+                    }
+                    if(args[0] == 'get') {
+                        get(ref(db, 'users/' + currentID)).then((data) => {
+                            if(data.val().pesan) {
+                                line.innerHTML = `Your pesan Is<br/><i class="code"></i>`;
+                                line.querySelector('.code').innerText = data.val().pesan;
+                                box.scrollTop = box.scrollHeight;
+                            } else {
+                                line.innerHTML = `You Dont have any pesan`;
+                                box.scrollTop = box.scrollHeight;
+                            }
+                        });
+                    } else if(args[0] == 'edit') {
+                        line.innerHTML = `<i>Waiting . . .</i>`;
+                        popup.prompt({
+                            msg: 'Your pesan is:',
+                            textarea: true,
+                            max: 100,
+                            onyes: (res) => {
+                                if(res.length > 150) {
+                                    line.innerHTML = `<span class="c-red">Please Enter less</span>`;
+                                } else {
+                                    update(ref(db, 'users/' + currentID), {pesan: res}).then(() => {
+                                        line.innerHTML = `<span class="c-green">update</span>`;
+                                    }).catch((err) => {
+                                        line.innerHTML = `<span class="c-red">Please enter less</span>`;
+                                    });
+                                }
+                            },
+                            onno: () => {
+                                line.innerHTML = `<span class="c-yellow">You cancled Edited Your pesan</span>`;
+                            }
+                        })
+                    } else if(args[0] == 'remove') {
+                        commands.last_used = 'pesan';
+                        line.innerHTML = `Type <span class="code">/confirm</span> to clear pesan`;
+                    } else {
+                        line.innerHTML = `<span class="c-red"><b>${args[0].replace(/\</g, '&lt;')}</b> is not valid</span>`;
+                    }
+                    box.appendChild(bot);
+                },
+                commit: (args, line, bot, box) => {
+                    commands.last_used = null;
+                    line.innerHTML = `<span class="c-yellow">Removing...</span>`;
+                    remove(ref(db, 'users/' + currentID + '/realname')).then(() => {
+                        line.innerHTML = `<span class="c-green">Cleared</span>`;
+                    })
+                    box.appendChild(bot);
+                }
+            },
+            website: {
+                init: (args, line, bot, box) => {
+                    commands.last_used = null;
+                    line.innerHTML = `Thinking...`;
+                    if(!args[0])  {
+                        line.innerHTML = `Usage: <br/>/website get<br/>/website edit<br/>/website remove`;
+                        return box.appendChild(bot);
+                    }
+                    if(args[0] == 'get') {
+                        get(ref(db, 'users/' + currentID)).then((data) => {
+                            if(data.val().website) {
+                                line.innerHTML = `Your website Is<br/><i class="code"></i>`;
+                                line.querySelector('.code').innerText = data.val().website;
+                                box.scrollTop = box.scrollHeight;
+                            } else {
+                                line.innerHTML = `You Dont have any website`;
+                                box.scrollTop = box.scrollHeight;
+                            }
+                        });
+                    } else if(args[0] == 'edit') {
+                        line.innerHTML = `<i>Waiting . . .</i>`;
+                        popup.prompt({
+                            msg: 'Your website is:',
+                            textarea: true,
+                            max: 100,
+                            onyes: (res) => {
+                                if(res.length > 150) {
+                                    line.innerHTML = `<span class="c-red">Please Enter less</span>`;
+                                } else {
+                                    update(ref(db, 'users/' + currentID), {website: res}).then(() => {
+                                        line.innerHTML = `<span class="c-green">update</span>`;
+                                    }).catch((err) => {
+                                        line.innerHTML = `<span class="c-red">Please enter less</span>`;
+                                    });
+                                }
+                            },
+                            onno: () => {
+                                line.innerHTML = `<span class="c-yellow">You cancled Edited Your website</span>`;
+                            }
+                        })
+                    } else if(args[0] == 'remove') {
+                        commands.last_used = 'website';
+                        line.innerHTML = `Type <span class="code">/confirm</span> to clear website`;
+                    } else {
+                        line.innerHTML = `<span class="c-red"><b>${args[0].replace(/\</g, '&lt;')}</b> is not valid</span>`;
+                    }
+                    box.appendChild(bot);
+                },
+                commit: (args, line, bot, box) => {
+                    commands.last_used = null;
+                    line.innerHTML = `<span class="c-yellow">Removing...</span>`;
+                    remove(ref(db, 'users/' + currentID + '/realname')).then(() => {
                         line.innerHTML = `<span class="c-green">Cleared</span>`;
                     })
                     box.appendChild(bot);
@@ -266,12 +438,34 @@ export const botCommand = (currentID) => {
                     });
                 }
             },
+            move: {
+                init: (args, line, bot, box) => {
+                    commands.last_used = 'move';
+                    line.innerHTML = `If You write <b>Move</b> you will be redirect to Tokko Realtime and Logout on <b>Tokko Chat</b> too! <br/>Type <span class="code">/confirm</span> to Continue`;
+                    box.appendChild(bot);
+                },
+                commit: (args, line, bot, box) => {
+                    commands.last_used = null;
+                    line.innerHTML = `<span class="c-yellow">Logging out And Move to <b>Tokko Realtime</b>..</span>`
+                    signOut(auth).then(() => {
+                        window.location.href = `https://tokko-realtime.blogspot.com/p/profile.html`;
+                    });
+                }
+            },
             help: {
                 init: (args, line, bot, box) => {
                     commands.last_used = null;
-                    line.innerHTML = `Command Detail`;
+                    line.innerHTML = `Redirect To Team Support`;
                     box.appendChild(bot);
-                    window.open(`https://kirimin.devanka761.my.id/data/helper/command/command_usage.json`);
+                    window.open(`https://cdn.jsdelivr.net/gh/Tokko-Blogger/Script@main/command_usage.json`);
+                }
+            },
+            support: {
+                init: (args, line, bot, box) => {
+                    commands.last_used = null;
+                    line.innerHTML = `Redirect To Team Support`;
+                    box.appendChild(bot);
+                    window.location.href = `https://tokko-gpt.blogspot.com/p/chat.html?r=user&gid=744011`;
                 }
             },
             close: {
