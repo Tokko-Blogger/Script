9c52819e563c06aebd3c1b825764192a9fda4200
Helmy Kambuna Otaya (Tokko Blogger) <21377149+twinnybloggers@users.noreply.github.com>
2024-08-10 00:49:30 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBcTsdNM48Ts1t5JTNdDby8281e7s7REDE",
-  authDomain: "simforum-login.firebaseapp.com",
-  databaseURL: "https://simforum-login-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "simforum-login",
-  storageBucket: "simforum-login.appspot.com",
-  messagingSenderId: "311465979099",
-  appId: "1:311465979099:web:8ded10990bee13bfb770fe",
-  measurementId: "G-WP0XZT3CHT"
-};
-// BIAR GA KELAMAAN PAS MAU PANGGIL NANTI
-const app = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage(payload => {
     // Customize notification here
